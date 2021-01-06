09223c3a9fd5cb88257634d34488f79b6b43b1e0
OUIYA Ismael <ismaelouiya@gmail.com>
2021-01-06 13:16:53 +0000

    ver

--- bootrsap4/app.js
+++ bootrsap4/app.js
@@ -1,12 +1,3 @@
-var options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: .1
-  }
-  
-  const handleIntersect = function() {
-      console.log('handleIntersect')
-  }
-
- const observer = new IntersectionObserver(handleIntersect, options);
- observer.observe(document.querySelector('.reveal'))
+function cool () {
+    alert('Présentation');
+}
